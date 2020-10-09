5fc7bbecbe01d767a90b8bd91f52964223d33c6f
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-10-09 12:26:47 -0400

    added model rating and bundle installed gem

---
+++ gamer-backend/db/migrate/20201009162620_create_ratings.rb
@@ -0,0 +1,11 @@
+class CreateRatings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ratings do |t|
+      t.integer :game_id
+      t.float :number
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
