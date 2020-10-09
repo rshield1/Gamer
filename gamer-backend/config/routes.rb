c59b69bf75a2b153629ade1cc91967b99f8395a0
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-10-09 12:39:08 -0400

    namespaced files for api calls

--- gamer-backend/config/routes.rb
+++ gamer-backend/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :ratings
-  resources :games
+
+  namespace :api do
+    namespace :v1 do
+        resources :ratings
+        resources :games
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
