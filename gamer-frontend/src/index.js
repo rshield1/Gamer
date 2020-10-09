4448f6b67729e9f0804092f0f255bb216c705baf
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-10-09 14:11:28 -0400

    added store and setup middleware

--- gamer-frontend/src/index.js
+++ gamer-frontend/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './App';
 
+//set up store
+const initialState = {}
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+)
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
