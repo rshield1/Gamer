9a2e1caddbbe748bc17227828421a83aff385101
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-10-09 13:05:45 -0400

    Added methods in controller

--- gamer-backend/app/controllers/api/v1/games_controller.rb
+++ gamer-backend/app/controllers/api/v1/games_controller.rb
@@ -8,15 +8,22 @@ class Api::V1::GamesController < ApplicationController
 
 
     def create
-
+        @game = Game.new(game_params)
+            if @game.save
+                render json: @game
+            else
+                render json: {error: 'Error Creating account'}
+            end
     end
 
     def show
-
+        @game = Game.find(params[:id])
+        render json: @game
     end
 
     def destroy 
-
+        @game = Game.find(params[:id])
+        @game.destroy
     end
 
     private
